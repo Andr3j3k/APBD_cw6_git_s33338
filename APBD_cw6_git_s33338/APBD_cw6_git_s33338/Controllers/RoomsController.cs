d6e7be6b1d023753dce77a91d9edbba1077399c5
Andrzej Pawlak <s33338@pjwstk.edu.pl>
2026-04-13 18:42:00 +0200

    RoomsController.cs update

--- APBD_cw6_git_s33338/APBD_cw6_git_s33338/Controllers/RoomsController.cs
+++ APBD_cw6_git_s33338/APBD_cw6_git_s33338/Controllers/RoomsController.cs
@@ -1,4 +1,6 @@
-﻿using APBD_cw6_git_s33338.Interfaces;
+﻿using APBD_cw6_git_s33338.DTOs;
+using APBD_cw6_git_s33338.Exceptions;
+using APBD_cw6_git_s33338.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APBD_cw6_git_s33338.Controllers;
@@ -7,5 +9,73 @@ namespace APBD_cw6_git_s33338.Controllers;
 [Route("api/[controller]")]
 public class RoomsController(IRoomService service) : ControllerBase
 {
-    
+    [HttpGet]
+    public IActionResult GetAll(
+        [FromQuery] int? minCapacity,
+        [FromQuery] bool? hasProjector,
+        [FromQuery] bool? activeOnly)
+    {
+        return Ok(service.GetAll(minCapacity, hasProjector, activeOnly));
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult GetById([FromRoute] int id)
+    {
+        try
+        {
+            return Ok(service.GetById(id));
+        }
+        catch (RoomNotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+
+    [HttpGet("building/{buildingCode}")]
+    public IActionResult GetByBuilding([FromRoute] string buildingCode)
+    {
+        return Ok(service.GetByBuilding(buildingCode));
+    }
+
+    [HttpPost]
+    public IActionResult Add([FromBody] CreateRoomDto room)
+    {
+        var createdRoom=service.Add(room);
+        
+        return CreatedAtAction(
+            nameof(GetById),
+            new {id=createdRoom.Id},
+            createdRoom);
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult Update([FromRoute] int id, [FromBody] UpdateRoomDto room)
+    {
+        try
+        {
+            return Ok(service.Update(id, room));
+        }
+        catch (RoomNotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        try
+        {
+            service.Remove(id);
+            return NoContent();
+        }
+        catch (RoomNotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (RoomDeleteConflictException e)
+        {
+            return Conflict(e.Message);
+        }
+    }
 }
